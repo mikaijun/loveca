eeb6e217beebd0483f53e59b466b9b5dfbd57051
mikaijun <mj.tempe443@gmail.com>
2025-02-16 06:20:52 +0900

    style: コード整形ツール導入
    eslintとprettierを導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,43 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+    "plugin:@next/next/recommended",
+  ),
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "import/order": "error",
+      "no-console": [
+        "error",
+        {
+          allow: ["error"],
+        },
+      ],
+      "react/jsx-sort-props": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "no-multi-spaces": "error",
+      "react/jsx-indent": ["error", 2],
+      "react/jsx-indent-props": ["error", 2],
+    },
+  },
 ];
 
 export default eslintConfig;
